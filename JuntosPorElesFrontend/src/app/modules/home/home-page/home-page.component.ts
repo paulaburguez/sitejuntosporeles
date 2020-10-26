31145903a252fcdf99321d0c662b769c1f097cc4
LaisMSantana <lais.santana123@hotmail.com>
2020-10-26 19:20:50 -0300

    Carrossel e textos tela inicial

--- JuntosPorElesFrontend/src/app/modules/home/home-page/home-page.component.ts
+++ JuntosPorElesFrontend/src/app/modules/home/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  public carouselImages = [{
+    image: 'assets/images/exemplo.png',
+    id: "1"
+  },
+  {
+    image: 'assets/images/Cachorro-comendo-melancia.jpg',
+    id: "2"
+  },
+  ];
+
+  carouselBasic: OwlOptions = {
+    items: 1,
+    autoHeight: true,
+    loop: true, 
+    nav: false,  
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+  }
 
   constructor() { }
 
--- JuntosPorElesFrontend/src/app/modules/home/home.module.ts
+++ JuntosPorElesFrontend/src/app/modules/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeRoutingModule } from './home-rounting.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule
   ]
 })
 export class HomeModule { }
